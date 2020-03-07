7a42fbc712b90482bb9fc1f54f858ac01a33cc31
Yuki Saito <gperson22@gmail.com>
2020-03-07 01:24:46 -0600

    Save Part 2 Changes

--- project-3/src/bin/kvs-server.rs
+++ project-3/src/bin/kvs-server.rs
@@ -1,17 +1,22 @@
 use kvs::Result;
 
 use clap::{load_yaml, App};
+use log::{info, LevelFilter};
 
 use std::net::SocketAddr;
 
 fn main() -> Result<()> {
+    env_logger::builder().filter_level(LevelFilter::Info).init();
+
     let yaml = load_yaml!("cli-server.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let _addr: SocketAddr = matches.value_of("addr").unwrap().parse()?;
-    let _engine = matches.value_of("engine").unwrap();
+    let addr: SocketAddr = matches.value_of("addr").unwrap().parse()?;
+    let engine = matches.value_of("engine").unwrap();
 
-    unimplemented!();
+    info!("kvs-server {}", env!("CARGO_PKG_VERSION"));
+    info!("Storage engine: {}", engine);
+    info!("Listening on {}", addr);
 
     Ok(())
 }
