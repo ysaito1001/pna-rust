deb280833b3b8880591cf2c8a3eecb61e0858a00
Yuki Saito <gperson22@gmail.com>
2020-04-11 01:10:07 -0500

    Save Part 4 Changes

--- project-4/src/thread_pool/shared_queue.rs
+++ project-4/src/thread_pool/shared_queue.rs
@@ -1,18 +1,54 @@
 use super::ThreadPool;
-
 use crate::Result;
 
-pub struct SharedQueueThreadPool;
+use crossbeam::channel::{self, Receiver, Sender};
+use log::{debug, error};
+
+use std::thread;
+
+pub struct SharedQueueThreadPool {
+    tx: Sender<Box<dyn FnOnce() + Send + 'static>>,
+}
 
 impl ThreadPool for SharedQueueThreadPool {
-    fn new(_number_of_threads: u32) -> Result<Self> {
-        unimplemented!();
+    fn new(number_of_threads: u32) -> Result<Self> {
+        let (tx, rx) = channel::unbounded();
+        for _ in 0..number_of_threads {
+            let rx = TaskReceiver(rx.clone());
+            thread::Builder::new().spawn(move || run_task(rx))?;
+        }
+        Ok(SharedQueueThreadPool { tx })
     }
 
-    fn spawn<F>(&self, _job: F)
+    fn spawn<F>(&self, job: F)
     where
         F: FnOnce() + Send + 'static,
     {
-        unimplemented!();
+        self.tx.send(Box::new(job)).unwrap();
+    }
+}
+
+#[derive(Clone)]
+struct TaskReceiver(Receiver<Box<dyn FnOnce() + Send + 'static>>);
+
+impl Drop for TaskReceiver {
+    fn drop(&mut self) {
+        if thread::panicking() {
+            let rx = self.clone();
+            if let Err(e) = thread::Builder::new().spawn(move || run_task(rx)) {
+                error!("Failed to spawn a thread: {}", e)
+            }
+        }
+    }
+}
+
+fn run_task(rx: TaskReceiver) {
+    loop {
+        match rx.0.recv() {
+            Ok(task) => {
+                task();
+            }
+            Err(_) => debug!("Thread exits because the thread pool is destroyed."),
+        }
     }
 }
